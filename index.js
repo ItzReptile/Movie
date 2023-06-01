7614009a80834a08f6d2a796d24f898c53d10704
jose <hernandezjos1121@gmail.com>
2023-05-31 21:05:25 -0400

    javascript

--- index.js
+++ index.js
@@ -1,67 +1,77 @@
 // api `https://www.omdbapi.com/?i=tt3896198&apikey=c5a8f649&`
+const movie = document.querySelector(".movie-flex");
+const Searchresults = document.querySelector(".search-results-title");
 
-const movie = document.querySelector(".movie-list");
-const searchresults = document.querySelector(".search-results-title");
-const headerEl = document.querySelector("#Movies");
-
-async function rendermovies(filter, search) {
+async function rendermovies(filter, Search) {
   movie.classList += " movie__loading";
   const movies = await fetch(
-    `https://www.omdbapi.com/?i=tt3896198&apikey=c5a8f649&s=${search}`
+    `https://www.omdbapi.com/?i=tt3896198&apikey=c5a8f649&s=${Search}`
   );
   movie.classList.remove("movie__loading");
   const moviesData = await movies.json();
   console.log(moviesData);
-  console.log(movie)
-  console.log(searchresults)
+  console.log(movie);
+  console.log(Searchresults);
   
-
-  if (moviesData.search) {
+  if (moviesData.Search) {
     if (filter === "NEWEST-OLDEST") {
-      moviesData.search.sort((a, b) => parseInt(b.Year) - parseInt(a.Year));
+      moviesData.Search.sort((a,b) => parseInt(b.Year) - parseInt(a.Year)
+      )
     } else if (filter === "OLDEST-NEWEST") {
-      moviesData.search.sort((a, b) => parseInt(a.Year) - parseInt(b.Year));
+      moviesData.Search.sort((a,b) => parseInt(a.Year) - parseInt(b.Year)
+      );
     }
-    movie.innerHTML = moviesData.search
-      .map((movie) => moviesHTML(movie))
-      .join("");
-    headerEl.scrollIntoView({ behavior: "smooth" });
+    movie.innerHTML = moviesData.Search.map((moviess) => moviesHTML(moviess))
   }
+  
 }
 
+
+
 function onSearchChange(event) {
-  const search = event.target.value;
-  searchresults.innerHTML = search;
-  rendermovies("", search);
+  const Search = event.target.value;
+  Searchresults.innerHTML = Search;
+  rendermovies("", Search);
+
 }
 
 function filtermovies(event) {
   const filter = event.target.value;
-  const search = searchresults.innerHTML;
-  rendermovies(filter, search);
+  const Search = Searchresults.innerHTML;
+  rendermovies(filter, Search);
+  if(arr.length >= 5){
+    arr.slice(0,5)
+  }
+  
 }
 
-function moviesHTML(HOPE) {
-  return `<div class="movie">
-  <figure class="movie-explanation">
-    <img
-      class="movie-img"
-      src="${HOPE.Poster}"
-      alt="Not Avaiable"
-    />
-    <div class="movie-background"></div>
-    <p class="movie-details">${HOPE.Plot}</p>
-  </figure>
-  <div class="movie-description">
-    <h1 class="movie-title">${HOPE.Title}</h1>
-    <h4 class="movie-rating yellow">${HOPE.imdbRatin}</h4>
-    <h3 class="movie-released">
-      <span class="movie-release-date">${HOPE.Released}</span>
-    </h3>
+
+
+
+function moviesHTML(moviess) {
+  return `<div class="movie-list">
+  <div class="movie" >
+    <figure class="movie-explanation">
+      <img
+        class="movie-img"
+        src="${moviess.Poster}"
+        alt="image not found"
+      />
+      <div class="movie-background"></div>
+      <p class="movie-details">MORE INFO</p>
+    </figure>
+    <div class="movie-description">
+      <h1 class="movie-title">${moviess.Title}</h1>
+      <h3 class="movie-released">
+        <span class="movie-release-date">${moviess.Year}</span>
+      </h3>
+    </div>
   </div>
-</div>`.join()
+</div>`;
 }
+
+
+
 setTimeout(() => {
   rendermovies("", "");
 });
-
